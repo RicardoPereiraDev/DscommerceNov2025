705a8e967df56503fb834b2eb3c5d08f94b36bac
RicardoPereiraDev <odracirDavidPereira@gmail.com>
2026-03-16 15:32:16 +0000

    created package Projections

---
+++ src/main/java/com/devsuperior/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,4 @@
+package com.devsuperior.dscommerce.projections;
+
+public interface UserDetailsProjection {
+}
--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.repositories;
 
 import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
