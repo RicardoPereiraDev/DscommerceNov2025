542d0a2c2ef20aa01c80c15d6406f4ba2ff3b419
RicardoPereiraDev <odracirDavidPereira@gmail.com>
2026-03-26 20:06:04 +0000

    BCrypt password

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -133,27 +133,27 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 
 }
--- src/main/java/com/devsuperior/dscommerce/services/AuthService.java
+++ src/main/java/com/devsuperior/dscommerce/services/AuthService.java
@@ -3,7 +3,9 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
 
     @Autowired
